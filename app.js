72d28a43a753514ddeccf63d380068501d7fbf6f
pmuradian <p.muradian@gmail.com>
2022-03-22 16:55:30 +0400

    Application setup

---
+++ app.js
@@ -0,0 +1,24 @@
+const { App, subtype } = require('@slack/bolt');
+
+
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  socketMode: true,
+  appToken: process.env.SLACK_APP_TOKEN,
+  // Socket Mode doesn't listen on a port, but in case you want your app to respond to OAuth,
+  // you still need to listen on some port!
+  port: process.env.PORT || 3000
+});
+
+app.event('message', async ({ message, say }) => {
+  // say() sends a message to the channel where the event was triggered
+  await say(`Hey there <@${message.user}>!`);
+});
+
+(async () => {
+  // Start your app
+  await app.start();
+
+  console.log('⚡️ Bolt app is running!');
+})();
