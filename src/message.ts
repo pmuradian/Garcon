791472c5c44179a25eb9965c84544bdb464bee8f
pmuradian <p.muradian@gmail.com>
2022-03-24 00:32:47 +0400

    Add url validator for buy.am domains

---
+++ src/message.ts
@@ -0,0 +1,15 @@
+
+// wake up message
+
+// order message
+
+// confirmation message
+
+// cancel message
+
+// URL validator for https://buy.am/ urls
+export function validateURL(url: string): boolean {
+    let result = url.match(/(http(s)?:\/\/.)(www\.)?[buy\.am\/-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);    
+    console.log(result);
+    return result != null;
+}
